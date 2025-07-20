2e90c38e6bbdebf1a46b32cb4fbbb87311b6e9d9
chungi21 <wlsgml0156@gmail.com>
2025-07-20 14:01:07 +0900

    state, useState

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,45 +1,23 @@
+import { useState } from "react";
+
 export default function Hello() {
 
-    function showName() {
-        console.log("Jinny");
-    }
+    // let name = "Jinny";
+    const [name, setName] = useState('Jinny');
 
-    function showAge(age) {
-        console.log(age);
-    }
+    function chaneName() {
+        // name = name == "Jinny" ? "jin" : "Jinny";
+        // document.getElementById("name").innerText = name;
+        const newName = name == "Jinny" ? "jin" : "Jinny";
+        setName(newName);
 
-    function showText1(e) {
-        console.log(e.target.value)
-    }
-
-    function showText2(txt) {
-        console.log(txt)
     }
 
     return (
         <>
-            <h1>Hello</h1>
-            <button onClick={showName}>show name</button>
-
-            <button onClick={
-                () => {
-                    console.log(20);
-                }
-            }>show age1</button>
-            
-            <button onClick={
-                () => {
-                    showAge(10);
-                }
-            }>show age2</button>
-
-            <br/>
-            <input type="text" onChange={showText1}/>
-            <br/>
-            <input type="text" onChange={ e => {
-                const txt = e.target.value;
-                showText2(txt);
-            }}/>
+            <h1>state</h1>
+            <h2 id="name">{name}</h2>            
+            <button onClick={chaneName}>Change</button>
         </>
     );
 }
