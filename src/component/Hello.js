e39f5ad9ca03da06e320e1879e288d0dda24fdbd
chungi21 <wlsgml0156@gmail.com>
2025-07-20 13:39:54 +0900

    이벤트 처리(Handling Events)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,16 +1,45 @@
-import World from "./World";
-import styles from "./Hello.module.css";
-
 export default function Hello() {
+
+    function showName() {
+        console.log("Jinny");
+    }
+
+    function showAge(age) {
+        console.log(age);
+    }
+
+    function showText1(e) {
+        console.log(e.target.value)
+    }
+
+    function showText2(txt) {
+        console.log(txt)
+    }
+
     return (
         <>
-            <h1 style={{
-                color: 'red',
-                borderBottom: '3px solid blue',
-                marginBottom: '40px', 
-                opacity: 0.5
-            }}>Hello</h1>
-            <div className={styles.box}>Hello</div>
+            <h1>Hello</h1>
+            <button onClick={showName}>show name</button>
+
+            <button onClick={
+                () => {
+                    console.log(20);
+                }
+            }>show age1</button>
+            
+            <button onClick={
+                () => {
+                    showAge(10);
+                }
+            }>show age2</button>
+
+            <br/>
+            <input type="text" onChange={showText1}/>
+            <br/>
+            <input type="text" onChange={ e => {
+                const txt = e.target.value;
+                showText2(txt);
+            }}/>
         </>
     );
 }
